4f54fc07a29ee0d8797b7b91e6067efff4435cec
dianannperla <waafirmelyza@hotmail.com>
2025-08-15 05:10:23 +0100

    feat: initial commit for src/components/SocialLinks.js

---
+++ src/components/SocialLinks.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function SocialLinks() {
+  return (
+    <div className="social-links">
+      <h2>Mạng Xã Hội</h2>
+      <ul>
+        <li><a href="https://facebook.com">Facebook</a></li>
+        <li><a href="https://linkedin.com">LinkedIn</a></li>
+        <li><a href="https://twitter.com">Twitter</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default SocialLinks;
