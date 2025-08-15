83fc9492d9dfaede2a8448561651e60b5ff42a2a
dianannperla <waafirmelyza@hotmail.com>
2025-08-15 05:10:18 +0100

    feat: initial commit for src/components/Profile.js

---
+++ src/components/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Profile() {
+  return (
+    <div className="profile">
+      <h1>Tên của bạn</h1>
+      <p>Mô tả về bản thân và công việc của bạn.</p>
+    </div>
+  );
+}
+
+export default Profile;
